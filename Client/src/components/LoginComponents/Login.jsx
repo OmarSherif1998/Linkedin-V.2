cb194588ca03e37550996dca30200fe5da29b3e5
OmarSherif1998 <omarmohamedsherif98@gmail.com>
2024-09-07 15:45:49 +0300

    Create Login.jsx

---
+++ Client/src/components/LoginComponents/Login.jsx
@@ -0,0 +1,24 @@
+/** @format */
+
+import Carousel from './Carousel';
+import Connect from './Connect';
+import Find from './Find';
+import LoginForm from './LoginForm';
+import GetStarted from './GetStarted';
+import Internship from './Internship';
+import PostJob from './PostJob';
+
+function Login() {
+	return (
+		<div>
+			<LoginForm />
+			<Internship />
+			<PostJob />
+			<Carousel />
+			<Connect />
+			<Find />
+			<GetStarted />
+		</div>
+	);
+}
+export default Login;
