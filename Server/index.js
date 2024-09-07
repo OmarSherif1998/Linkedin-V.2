19eed727a73c6247f543c1c4a14029b72f0610c1
OmarSherif1998 <omarmohamedsherif98@gmail.com>
2024-09-07 15:42:54 +0300

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -4,17 +4,21 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import userRoute from './routers/userRouter.js'; // Import userRoute
+import userRouter from './routers/userRouter.js';
+import fileRouter from './routers/fileRouter.js';
+import registrationRouter from './routers/registrationRouter.js';
+import postRouter from './routers/postRouter.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001; // Set port number
+const PORT = process.env.PORT || 3001;
 //Middleware
 app.use(express.json());
 app.use(
 	cors({
 		origin: `http://localhost:3000`,
+		credentials: true,
 	})
 );
 
@@ -33,5 +37,7 @@ mongoose
 	});
 
 //Routers
-
-app.use('/users', userRoute);
+app.use('/users', userRouter);
+app.use('/files', fileRouter);
+app.use('/regi', registrationRouter);
+app.use('/post', postRouter);
