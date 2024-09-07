668a9fc8f3dc66a92502180e72009d094c063037
OmarSherif1998 <omarmohamedsherif98@gmail.com>
2024-09-07 15:44:53 +0300

    Update index.js

--- Client/src/index.js
+++ Client/src/index.js
@@ -2,12 +2,19 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './CSS/index.css';
 import App from './pages/App';
+import './CSS/index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Redux/store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<Router>
+				<App />
+			</Router>
+		</Provider>
 	</React.StrictMode>
 );
