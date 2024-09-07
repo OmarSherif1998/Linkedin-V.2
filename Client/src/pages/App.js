51f09741df589ddd2eaaff4392dbd95d4f83173f
OmarSherif1998 <omarmohamedsherif98@gmail.com>
2024-09-07 15:44:48 +0300

    Update App.js

--- Client/src/pages/App.js
+++ Client/src/pages/App.js
@@ -1,42 +1,85 @@
 /** @format */
 
-//import './CSS/App.css';
-import Login from './Login';
-import LHeader from '../components/LHeader';
-import Footer from '../components/Footer.jsx';
-import Feed from '../pages/Feed';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { login, logout, selectUser } from '../Redux/sllices/userSlice.js';
+import { fetchUserData } from '../api/users/userAPI.js';
+import { useHandlers } from '../hooks/useHandlers.js';
+import SignUp from './SignUp.jsx';
+import Profile from './Profile.jsx';
 import Header from '../components/Header';
-import Sidebar from '../components/Sidebar';
-import Connection from '../components/Connection.jsx';
-//import BinarySort from './BinarySort.jsx';
+import Home from './Home.jsx';
+import LandingPage from './LandingPage.jsx';
+import LoadingScreen from '../components/LoadingScreen.jsx';
 
 function App() {
-	const user = true;
+	const user = useSelector(selectUser);
+	const dispatch = useDispatch();
+	const { loading, setLoading } = useHandlers();
+
+	useEffect(() => {
+		const checkAuth = async () => {
+			try {
+				setLoading(true);
+				const token = localStorage.getItem('token');
+
+				if (token) {
+					const userData = await fetchUserData(token);
+
+					if (userData) {
+						dispatch(login(userData));
+					} else {
+						localStorage.removeItem('token');
+						dispatch(logout());
+						Navigate('/login');
+					}
+				} else {
+					dispatch(logout());
+					localStorage.removeItem('token'); // could cause issue if the token already removed, not sure though
+					Navigate('/login');
+				}
+			} catch (error) {
+				console.error('Error fetching user data:', error);
+				localStorage.removeItem('token');
+
+				dispatch(logout());
+			} finally {
+				setLoading(false); // Stop loading regardless of outcome
+			}
+		};
+
+		checkAuth();
+	}, [dispatch, setLoading]);
+
+	if (loading) return <LoadingScreen />;
 
 	return (
 		<div
 			className={`flex flex-col items-center min-h-screen ${
-				user ? 'bg-BgColor' : ' bg-white'
+				user ? 'bg-BgColor' : 'bg-white'
 			}`}
 		>
 			{!user ? (
-				<>
-					<LHeader />
-					<Login />
-					<Footer />
-				</>
+				<Routes>
+					<Route path='/' element={<Navigate to='/login' />} />
+					<Route path='/login' element={<LandingPage />} />
+					<Route path='/signup' element={<SignUp />} />
+					<Route path='/*' element={<Navigate to='/login' />} />
+				</Routes>
 			) : (
 				<>
 					<Header />
-					<div className='flex justify-center mt-[1.875rem] gap-[1.25rem] w-[90%]  '>
-						<Sidebar />
-						<Feed />
-						{/* <BinarySort /> */}
-						<Connection />
-					</div>
+					<Routes>
+						<Route path='/' element={<Navigate to='/home' />} />
+						<Route path='/home' element={<Home />} />
+						<Route path='/profile' element={<Profile />} />
+						<Route path='/login' element={<Navigate to='/home' />} />
+					</Routes>
 				</>
 			)}
 		</div>
 	);
 }
+
 export default App;
