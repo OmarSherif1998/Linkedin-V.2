0dcfac9cc1a14a232488f30e59ee1a04f53674dd
OmarSherif1998 <omarmohamedsherif98@gmail.com>
2024-09-07 15:42:03 +0300

    Created the  userRouter

--- Server/routers/userRouter.js
+++ Server/routers/userRouter.js
@@ -1,15 +1,16 @@
 /** @format */
 
 import express from 'express';
-import User from '../api/db/models/user.js';
+import User from '../schema/user.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-
+import authenticateToken from '../middlewares/authenticateToken.js';
 const userRouter = express.Router();
 
 userRouter.get('/', async (req, res) => {
 	try {
-		const users = await User.find({});
+		const users = await User.find({}).select('-password');
+		console.log(users);
 		res.status(200).json(users);
 	} catch (err) {
 		res.status(500).json({ message: 'Error fetching users', error: err });
@@ -18,28 +19,34 @@ userRouter.get('/', async (req, res) => {
 
 userRouter.post('/authenticateUser', async (req, res) => {
 	const { email, password } = req.body;
-
 	try {
 		const user = await User.findOne({ email });
+		if (!user) return res.status(401).json({ message: 'Invalid credentials' });
 
-		if (user.email === email) {
-			const salt = await bcrypt.genSalt(10);
-			const hashedPassword = user.password;
-			const isMatch = await bcrypt.compare(password, hashedPassword);
-			if (isMatch) {
-				//Generate JWT token
+		const hashedPassword = user.password;
+		const isMatch = await bcrypt.compare(password, hashedPassword);
+		if (!isMatch)
+			return res.status(401).json({ message: 'Invalid credentials' });
 
-				const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-					expiresIn: '1h',
-				});
-				res.json({ message: 'User authenticated successfully', token });
-			} else {
-				res.status(401).json({ message: 'Invalid credentials' });
-			}
-		}
+		const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+			expiresIn: '1h',
+		});
+		res.json({ message: 'User authenticated successfully', token });
 	} catch (error) {
 		console.error('Login error:', error);
 		res.status(500).json({ message: 'Server error' });
 	}
 });
+
+userRouter.get('/me', authenticateToken, async (req, res) => {
+	console.log('first');
+	try {
+		const userId = req.user.userId;
+		const user = await User.findById(userId).select('-password');
+		res.json(user);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
 export default userRouter;
