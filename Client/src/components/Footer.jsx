4febfb5a7451cbf2e0b9cebc8776d71b282a0304
OmarSherif1998 <omarmohamedsherif98@gmail.com>
2024-09-07 15:46:51 +0300

    Update Footer.jsx

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,8 +1,7 @@
 /** @format */
 
 import React from 'react';
-import InputOption from './InputOption';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { ColumnList, SubColumnList } from '../functions/footerFunctions.js';
 import {
 	column1,
 	column2,
@@ -10,46 +9,6 @@ import {
 	column4,
 	column5,
 } from '../staticData/footerData';
-const ColumnList = ({ data }) => {
-	return (
-		<ul>
-			{data.map((col, index) => (
-				<li
-					key={index}
-					className={`hover:cursor-pointer ${
-						col.title ? 'font-bold' : 'font-normal  hover:text-blue-900 '
-					}`}
-				>
-					{col.name}
-				</li>
-			))}
-		</ul>
-	);
-};
-const SubColumnList = ({ data }) => {
-	return (
-		<ul className='flex gap-[0.5rem]  pl-[1.25rem]'>
-			<li>
-				<img
-					src={require('../images/linkedin - black.png')}
-					alt=''
-					className='h-[1rem] mt-[0.25rem] w-[4.5rem]'
-				/>
-			</li>
-			{data.map((col, index) => (
-				<li
-					key={index}
-					className='font-normal cursor-pointer hover:text-blue-800'
-				>
-					{col.name}
-				</li>
-			))}
-			<li>
-				<InputOption Icon={KeyboardArrowDownIcon} />
-			</li>
-		</ul>
-	);
-};
 
 function Footer() {
 	return (
