2baf4ac737318e6a032c67dd63e222127ec24e72
OmarSherif1998 <omarmohamedsherif98@gmail.com>
2024-09-08 08:26:26 +0300

    Update Post.jsx

--- Client/src/components/Home/Post.jsx
+++ Client/src/components/Home/Post.jsx
@@ -11,8 +11,7 @@ import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import CloseIcon from '@mui/icons-material/Close';
 const Post = forwardRef(
-	({ name, bio, postTextBody, photoURL, postPictureUrl }, ref) => {
-		console.log(name);
+	({ name, bio, postTextBody, photoURL, postPictureUrl, data }, ref) => {
 		return (
 			<div
 				ref={ref}
@@ -23,11 +22,14 @@ const Post = forwardRef(
 					<CloseIcon className='text-black cursor-pointer' />
 				</div>
 				<div className='flex mb-[0.625rem]  '>
-					<Avatar src={photoURL} />
+					<Avatar src={data.profilePicture} />
 					<div className='ml-[0.625rem]'>
-						<h2 className='text-[0.9375rem] text-black font-normal'>{name}</h2>
-						<p className='text-[0.75rem] text-gray-500'>{bio}</p>
+						<h2 className='text-[0.9375rem] text-black font-normal'>
+							{data.username}
+						</h2>
+						<p className='text-[0.75rem] text-gray-500'>{data.bio}</p>
 					</div>
+
 					<div className='flex items-center gap-2 ml-auto font-medium text-blue-500   cursor-pointer hover:text-postButtonColor group hover:bg-blue-100 hover:bg-opacity-50 hover:rounded-xl px-[0.5rem]'>
 						<PersonAddIcon className='ml-auto text-blue-500 group-hover:text-postButtonColor' />
 						<p className='group-hover:text-postButtonColor'>Connect</p>
@@ -35,7 +37,7 @@ const Post = forwardRef(
 				</div>
 
 				<div className='break-words'>
-					<p className='ml-[4.375rem]'>{postTextBody}</p>
+					<p className='ml-[2rem] '>{data._doc.content}</p>
 				</div>
 
 				{postPictureUrl ? (
